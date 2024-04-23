c3e8f97b2eeb0ce071949bcf4cd443fc3cf8438c
Do Huy Hoang <hoangdh3@rikkeisoft.comm>
2024-04-23 16:58:08 +0700

    add transition animation between login/register

--- app/src/main/java/com/example/chatapp/presentation/screen/authentication/RegisterFragment.kt
+++ app/src/main/java/com/example/chatapp/presentation/screen/authentication/RegisterFragment.kt
@@ -1,11 +1,6 @@
 package com.example.chatapp.presentation.screen.authentication
 
 import androidx.fragment.app.viewModels
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.chatapp.R
 import com.example.chatapp.databinding.FragmentRegisterBinding
 import com.example.chatapp.domain.core.base.BaseFragment
